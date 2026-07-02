6e319bf3a304d6b0003c64d508b5f09ce95b3032
DevAJH89 <DevAJH89@gmail.com>
2026-07-02 17:10:35 +0900

    신고게시글작성

--- src/main/java/com/iso/plogues/report/model/dao/ReportMapper.java
+++ src/main/java/com/iso/plogues/report/model/dao/ReportMapper.java
@@ -1,8 +1,10 @@
 package com.iso.plogues.report.model.dao;
 
-import com.iso.plogues.report.model.dto.ReportDto;
+import org.apache.ibatis.annotations.Mapper;
+
 import com.iso.plogues.report.model.vo.Report;
 
+@Mapper
 public interface ReportMapper {
 	
 	int saveReport(Report report);
--- src/main/java/com/iso/plogues/report/model/dto/ReportDto.java
+++ src/main/java/com/iso/plogues/report/model/dto/ReportDto.java
@@ -2,6 +2,8 @@ package com.iso.plogues.report.model.dto;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +15,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ReportDto {
 
-	// 검증하기
 	private Long reportNo;
 	private String userId;
+	@NotBlank(message="분류를 입력해 주세요")
 	private String reportCategory;
 	private String boardType;
+	@NotBlank(message="내용을 입력해 주세요")
+	@Size(min=2, max=500, message="내용은 2글자 이상 500글자까지 작성가능합니다")
 	private String content;
 	private LocalDateTime creatDate;
 	private String update;
