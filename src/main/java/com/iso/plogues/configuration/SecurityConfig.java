d3a4921e9b52f06719073602d0c1e9565bb2755c
DevAJH89 <DevAJH89@gmail.com>
2026-07-29 10:33:35 +0900

    coffee 실습

--- src/main/java/com/iso/plogues/configuration/SecurityConfig.java
+++ src/main/java/com/iso/plogues/configuration/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+		configuration.setAllowedOrigins(Arrays.asList("http://54.180.107.6", "http://54.180.107.6:80", "http://localhost:5175")); 
 		configuration.setAllowedMethods(Arrays.asList("POST", "PATCH", "DELETE", "GET", "PUT", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 		configuration.setAllowCredentials(true);
